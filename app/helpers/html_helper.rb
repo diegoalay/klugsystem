07f1784dbd0aff9e652f87748d998193f11e7818
diego-alay <diego.alay@galileo.edu>
2022-02-07 05:56:42 +0100

    fix: asset validator in production

--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -1,7 +1,11 @@
 module HtmlHelper
 
   def controller_asset_exists?(controller_path, type)
-    Klugsystem::Application.assets.find_asset(controller_asset(controller_path, type))
+    if Rails.env == "development"
+      Klugsystem::Application.assets.find_asset(controller_asset(controller_path, type))
+    else
+      Rails.application.assets_manifest.find_sources(controller_asset(controller_path, type)).any?
+    end
   end
 
   def controller_asset(controller_path, type)
