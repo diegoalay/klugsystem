18c3ef49db66907352ba9a62701f4954c85d8541
diegoalay <diego.alay@galileo.edu>
2022-05-16 20:10:58 -0600

    fix: responsive design

--- app/vue/components/component-confirm-sale.vue
+++ app/vue/components/component-confirm-sale.vue
@@ -37,8 +37,8 @@ export default {
 
 <template>
     <section>
-        <b-row>
-            <b-col v-for="sale_type in sale_types" :key="sale_type.value">
+        <b-row v-if="sale_types">
+            <b-col :md="sale_types.length / 12" sm="12" v-for="sale_type in sale_types" :key="sale_type.value">
                 <b-button block variant="success" @click="submitSale(sale_type.value)">
                     <font-awesome-icon :icon="getIcon(sale_type.value)"/> &nbsp; {{ sale_type.text }}
                 </b-button>
