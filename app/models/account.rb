513024586cd0a53378a7f1ce8dce9651655b3be7
diegoalay <diego.alay@galileo.edu>
2022-06-15 08:01:52 -0600

    feta: transnufio data import

--- app/models/account.rb
+++ app/models/account.rb
@@ -50,6 +50,7 @@ class Account < ApplicationRecord
     clients.find_or_initialize_by(billing_identifier: 'CF') do |record|
       record.billing_address = ''
       record.billing_name = 'CF'
+      record.billing_address = '-'
       record.save(validate: false)
     end
   end
