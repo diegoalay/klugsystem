2aef1da4619965427caba941dbc068f1aacbb007
diegoalay <diego.alay@galileo.edu>
2022-06-23 01:29:31 -0600

    fix: billing field deletion

--- app/models/account/billing_field.rb
+++ app/models/account/billing_field.rb
@@ -5,4 +5,6 @@ class Account::BillingField < ApplicationRecord
   validates :color, presence: true
   validates :position_x, presence: true
   validates :position_y, presence: true
+
+  acts_as_paranoid
 end
