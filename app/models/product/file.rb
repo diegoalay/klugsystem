52b53b029a7d1db18cb30354d8c905ae38e8def1
diegoalay <diego.alay@galileo.edu>
2022-07-20 18:54:54 -0600

    fix: product file allowed extensions

--- app/models/product/file.rb
+++ app/models/product/file.rb
@@ -1,13 +1,14 @@
 class Product::File < Shared::File
-  belongs_to :object_model, class_name: "Product", foreign_key: "product_id"
+  belongs_to :object_model, class_name: 'Product', foreign_key: 'product_id'
 
   def self.allowed_file_extensions
     return [
-      "jpeg",
-      "png",
-      "jpg",
-      "gif",
-      "webp"
+      'jpeg',
+      'png',
+      'jpg',
+      'gif',
+      'webp',
+      'heif'
     ]
   end
 end
