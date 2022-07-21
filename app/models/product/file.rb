b7c4977640a736a4ec1cb750f3b507e6cba5ecae
diegoalay <diego.alay@galileo.edu>
2022-07-21 16:29:38 -0600

    feat: downcase file extension

--- app/models/product/file.rb
+++ app/models/product/file.rb
@@ -1,6 +1,8 @@
 class Product::File < Shared::File
   belongs_to :object_model, class_name: 'Product', foreign_key: 'product_id'
 
+  before_validation :normalize_file_extension, on: :create
+
   def self.allowed_file_extensions
     return [
       'jpeg',
@@ -10,5 +12,11 @@ class Product::File < Shared::File
       'webp',
       'heic'
     ]
+  end
+
+  private
+
+  def normalize_file_extension
+    self.file_extension = file_extension.downcase
   end
 end
